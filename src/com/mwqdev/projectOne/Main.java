72f2f32f94a94b3ea2e3c2af8ebe44bb5faefeda
mwqdev <quarles.michael@gmail.com>
2017-07-17 14:33:12 -0400

    changes

--- src/com/mwqdev/projectOne/Main.java
+++ src/com/mwqdev/projectOne/Main.java
@@ -10,14 +10,16 @@ public class Main {
 
         String firstString;
         String secondString;
-
         Double operand1;
         Double operand2;
 
         System.out.println("Please guess your first number");
+
         firstString = scanner.nextLine();
         operand1 = Double.parseDouble(firstString);
+
         System.out.println("Guess your second number");
+
         secondString = scanner.nextLine();
         operand2 = Double.parseDouble(secondString);
 
@@ -27,9 +29,11 @@ public class Main {
         Double multiplication = operand1 * operand2;
         Double remainder = operand1 % operand2;
 
-        System.out.println(function(sum, difference, division, multiplication, remainder));
+        System.out.println(showResults(sum, difference, division, multiplication, remainder));
     }
-    private static String function(double sum, double difference, double division, double multiplication, double remainder) {
-        return("The sum is " + sum + ". The difference is " + difference + ". The quotient is " + division + ". The Product is " + multiplication + ". Remainder is " + remainder + ".");
+
+    private static String showResults(double sum, double difference, double quotient, double product, double remainder) {
+        return String.format("The sum is %s. The difference is %s. The quotient is %s. The Product is %s. Remainder is %s.", sum, difference, quotient, product, remainder);
+//        return "The sum is " + sum + ". The difference is " + difference + ". The quotient is " + quotient + " The Product is " + product + ". Remainder is " + remainder + ".";
     }
 }
